9b73840b19eec34892fcbbe401345c3abd353a15
Keyssi Gochez <keyssiesme@gmail.com>
2021-08-03 23:23:46 -0600

    Agregar archivos de proyecto.

---
+++ WebBlog/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace WebBlog.Models
+{
+    public partial class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Author { get; set; }
+        public string Text { get; set; }
+    }
+}
